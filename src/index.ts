e6eaecd3860706c46c17af317b4deca649d80fd5
Todd Baert <toddbaert@gmail.com>
2022-05-10 14:13:40 -0400

    Add testing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
+// real code will go here, just scaffolding the project for now.
 export const greet = (greeting: string): string => {
-  console.log(greeting);
+  console.log(`${greeting}, OpenFeature`);
   return greeting;
 };
