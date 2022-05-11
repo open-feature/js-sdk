dac783faa8d463213a8b37943f0f7a89d51a63ba
Todd Baert <toddbaert@gmail.com>
2022-05-11 13:11:14 -0400

    Add linting

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,8 @@
-import { greet } from "../src/index";
+import { greet } from '../src/index';
 
-describe("greet", () => {
-  it("should return greeting", () => {
-    const result = greet("hi");
-    expect(result).toEqual("hi");
+describe('greet', () => {
+  it('should return greeting', () => {
+    const result = greet('hi');
+    expect(result).toEqual('hi');
   });
 });
