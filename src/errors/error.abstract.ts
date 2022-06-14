08e4ea93cfa1b99db0940938271431551ab44fc7
Weyert de Boer <weyert.deboer@tapico.io>
2022-06-14 19:58:11 +0100

    style: resolve linting/prettier issues

--- src/errors/error.abstract.ts
+++
@@ -1,5 +0,0 @@
-import type { ErrorCode } from './codes'
-
-export abstract class OpenFeatureError extends Error {
-  abstract code: ErrorCode
-}
--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,4 +1,4 @@
 export * from './codes.js';
-export * from './flag-not-found.error.js';
-export * from './parse.error.js';
-export * from './type-mismatch.error.js';
+export * from './flag-not-found-error.js';
+export * from './parse-error.js';
+export * from './type-mismatch-error.js';
