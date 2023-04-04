0eadda64e502a81be86a57aa14ffe6c76d6d7aca
realanna <anna.reale@dynatrace.com>
2023-04-04 11:27:30 +0200

    chore: added client suite to tests similar to the server version
    Signed-off-by: realanna <anna.reale@dynatrace.com>

--- packages/client/test/client.spec.ts
+++ packages/client/test/client.spec.ts
@@ -3,6 +3,8 @@ import {
   EvaluationContext,
   EvaluationDetails,
   JsonValue,
+  JsonArray,
+  JsonObject,
   ResolutionDetails,
   StandardResolutionReasons,
 
@@ -123,29 +125,180 @@ describe('OpenFeatureClient', () => {
       client = OpenFeature.getClient();
     });
 
-    describe('getBooleanDetails', () => {
-      it('should return boolean details, and call boolean resolver', async () => {
-        const booleanFlag = 'my-boolean-flag';
-        const defaultBooleanValue = false;
-        const booleanDetails = await client.getBooleanDetails(booleanFlag, defaultBooleanValue);
+    describe('flag evaluation', () => {
+      describe('getBooleanValue', () => {
+        it('should return boolean, and call boolean resolver', async () => {
+          const booleanFlag = 'my-boolean-flag';
+          const defaultBooleanValue = false;
+          const value = await client.getBooleanValue(booleanFlag, defaultBooleanValue);
 
-        expect(booleanDetails.value).toEqual(BOOLEAN_VALUE);
-        expect(booleanDetails.variant).toEqual(BOOLEAN_VARIANT);
-        expect(MOCK_PROVIDER.resolveBooleanEvaluation).toHaveBeenCalledWith(booleanFlag, defaultBooleanValue, {}, {});
+          expect(value).toEqual(BOOLEAN_VALUE);
+          expect(MOCK_PROVIDER.resolveBooleanEvaluation).toHaveBeenCalledWith(booleanFlag, defaultBooleanValue, {}, {});
+        });
+      });
+
+      describe('getStringValue', () => {
+        describe('with no generic arg (as string)', () => {
+          it('should return string, and call string resolver', async () => {
+            const stringFlag = 'my-string-flag';
+            const defaultStringValue = 'default-value';
+            const value: string = await client.getStringValue(stringFlag, defaultStringValue);
+
+            expect(value).toEqual(STRING_VALUE);
+            expect(MOCK_PROVIDER.resolveStringEvaluation).toHaveBeenCalledWith(stringFlag, defaultStringValue, {}, {});
+          });
+        });
+
+        describe('with generic arg', () => {
+          it('should return T, and call string resolver', async () => {
+            const stringFlag = 'my-string-flag';
+            type MyRestrictedString = 'val' | 'other';
+            const defaultStringValue = 'other';
+            const value: MyRestrictedString = await client.getStringValue<MyRestrictedString>(
+              stringFlag,
+              defaultStringValue
+            );
+
+            expect(value).toEqual(STRING_VALUE);
+            expect(MOCK_PROVIDER.resolveStringEvaluation).toHaveBeenCalledWith(stringFlag, defaultStringValue, {}, {});
+          });
+        });
+
+        describe('getNumberValue', () => {
+          describe('with no generic arg (as number)', () => {
+            it('should return number, and call number resolver', async () => {
+              const numberFlag = 'my-number-flag';
+              const defaultNumberValue = 1970;
+              const value: number = await client.getNumberValue(numberFlag, defaultNumberValue);
+
+              expect(value).toEqual(NUMBER_VALUE);
+              expect(MOCK_PROVIDER.resolveNumberEvaluation).toHaveBeenCalledWith(numberFlag, defaultNumberValue, {}, {});
+            });
+          });
+
+          describe('with generic arg', () => {
+            it('should return T, and call number resolver', async () => {
+              const numberFlag = 'my-number-flag';
+              type MyRestrictedNumber = 4096 | 2048;
+              const defaultNumberValue = 4096;
+              const value: MyRestrictedNumber = await client.getNumberValue<MyRestrictedNumber>(
+                numberFlag,
+                defaultNumberValue
+              );
+
+              expect(value).toEqual(NUMBER_VALUE);
+              expect(MOCK_PROVIDER.resolveNumberEvaluation).toHaveBeenCalledWith(numberFlag, defaultNumberValue, {}, {});
+            });
+          });
+        });
+
+        describe('getObjectValue', () => {
+          describe('with no generic arg (as JsonValue)', () => {
+            it('should return JsonValue, and call object resolver', async () => {
+              const objectFlag = 'my-object-flag';
+              const defaultObjectFlag = {};
+              const value: JsonValue = await client.getObjectValue(objectFlag, defaultObjectFlag);
+
+              // compare the object
+              expect(value).toEqual(OBJECT_VALUE);
+
+              // explore the object - type assertions required for safety.
+              const jsonObject: JsonObject = (value as JsonObject)[INNER_KEY] as JsonObject;
+              const nullValue = jsonObject?.[INNER_NULL_KEY] as null;
+              const booleanValue = jsonObject?.[INNER_BOOLEAN_KEY] as boolean;
+              const stringValue = jsonObject?.[INNER_STRING_KEY] as string;
+              const numberValue = jsonObject?.[INNER_NUMBER_KEY] as number;
+              const arrayValue = jsonObject?.[INNER_ARRAY_KEY] as JsonArray;
+
+              expect(nullValue).toEqual(null);
+              expect(booleanValue).toEqual(BOOLEAN_VALUE);
+              expect(stringValue).toEqual(STRING_VALUE);
+              expect(numberValue).toEqual(NUMBER_VALUE);
+              expect(arrayValue).toEqual(ARRAY_VALUE);
+            });
+          });
+
+          describe('with generic arg', () => {
+            it('should return T, and call object resolver', async () => {
+              const objectFlag = 'my-object-flag';
+
+              type MyType = {
+                inner: {
+                  booleanKey: boolean;
+                };
+              };
+
+              const defaultMyTypeFlag: MyType = {
+                inner: {
+                  booleanKey: false,
+                },
+              };
+              const value: MyType = await client.getObjectValue<MyType>(objectFlag, defaultMyTypeFlag);
+
+              const innerBooleanValue: boolean = value.inner.booleanKey;
+              expect(innerBooleanValue).toBeTruthy();
+            });
+          });
+        });
       });
     });
 
-    describe('getStringValue', () => {
-      describe('with no generic arg (as string)', () => {
-        it('should return string, and call string resolver', async () => {
-          const stringFlag = 'my-string-flag';
-          const defaultStringValue = 'default-value';
-          const value: string = await client.getStringValue(stringFlag, defaultStringValue);
+    describe('Requirement 1.4.1', () => {
+      let client: Client;
 
-          expect(value).toEqual(STRING_VALUE);
-          expect(MOCK_PROVIDER.resolveStringEvaluation).toHaveBeenCalledWith(stringFlag, defaultStringValue, {}, {});
+      beforeEach(() => {
+        client = OpenFeature.getClient();
+      });
+
+      describe('detailed flag evaluation', () => {
+        describe('getBooleanDetails', () => {
+          it('should return boolean details, and call boolean resolver', async () => {
+            const booleanFlag = 'my-boolean-flag';
+            const defaultBooleanValue = false;
+            const booleanDetails = await client.getBooleanDetails(booleanFlag, defaultBooleanValue);
+
+            expect(booleanDetails.value).toEqual(BOOLEAN_VALUE);
+            expect(booleanDetails.variant).toEqual(BOOLEAN_VARIANT);
+            expect(MOCK_PROVIDER.resolveBooleanEvaluation).toHaveBeenCalledWith(booleanFlag, defaultBooleanValue, {}, {});
+          });
+        });
+        describe('getStringDetails', () => {
+          it('should return string details, and call string resolver', async () => {
+            const stringFlag = 'my-string-flag';
+            const defaultStringValue = 'default-value';
+            const stringDetails = await client.getStringDetails(stringFlag, defaultStringValue);
+
+            expect(stringDetails.value).toEqual(STRING_VALUE);
+            expect(stringDetails.variant).toEqual(STRING_VARIANT);
+            expect(MOCK_PROVIDER.resolveStringEvaluation).toHaveBeenCalledWith(stringFlag, defaultStringValue, {}, {});
+          });
+        });
+
+        describe('getNumberDetails', () => {
+          it('should return number details, and call number resolver', async () => {
+            const numberFlag = 'my-number-flag';
+            const defaultNumberValue = 1970;
+            const numberDetails = await client.getNumberDetails(numberFlag, defaultNumberValue);
+
+            expect(numberDetails.value).toEqual(NUMBER_VALUE);
+            expect(numberDetails.variant).toEqual(NUMBER_VARIANT);
+            expect(MOCK_PROVIDER.resolveNumberEvaluation).toHaveBeenCalledWith(numberFlag, defaultNumberValue, {}, {});
+          });
+        });
+
+        describe('getObjectDetails', () => {
+          it('should return object details, and call object resolver', async () => {
+            const objectFlag = 'my-object-flag';
+            const defaultObjectFlag = {};
+            const objectDetails = await client.getObjectDetails(objectFlag, defaultObjectFlag);
+
+            expect(objectDetails.value).toEqual(OBJECT_VALUE);
+            expect(objectDetails.variant).toEqual(OBJECT_VARIANT);
+            expect(MOCK_PROVIDER.resolveObjectEvaluation).toHaveBeenCalledWith(objectFlag, defaultObjectFlag, {}, {});
+          });
         });
       });
+
     });
   });
 });
