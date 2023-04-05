de59be777adff65b5da768e983c02713c1712b85
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2023-04-05 11:27:55 +0200

    copy over server tests as boilerplate
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

---
+++ packages/client/integration/step-definitions/setup.ts
@@ -0,0 +1,14 @@
+import assert from 'assert';
+import { OpenFeature } from '../../src';
+import { FlagdProvider } from '@openfeature/flagd-provider';
+
+const FLAGD_NAME = 'flagd Provider';
+
+// register the flagd provider before the tests.
+console.log('Setting flagd provider...');
+OpenFeature.setProvider(new FlagdProvider());
+assert(
+  OpenFeature.providerMetadata.name === FLAGD_NAME,
+  new Error(`Expected ${FLAGD_NAME} provider to be configured, instead got: ${OpenFeature.providerMetadata.name}`)
+);
+console.log('flagd provider configured!');
