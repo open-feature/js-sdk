ef61cd411caad418846683c7cede671177eb8016
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2023-04-11 16:31:28 +0200

    add flagd-web-provider and use it in jest setup
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

--- packages/client/integration/step-definitions/setup.ts
+++ packages/client/integration/step-definitions/setup.ts
@@ -1,12 +1,17 @@
 import assert from 'assert';
 import { OpenFeature } from '../../src';
-import { FlagdProvider } from '@openfeature/flagd-provider';
+import { FlagdWebProvider } from '@openfeature/flagd-web-provider';
 
 const FLAGD_NAME = 'flagd Provider';
 
 // register the flagd provider before the tests.
 console.log('Setting flagd provider...');
-OpenFeature.setProvider(new FlagdProvider());
+OpenFeature.setProvider(
+  new FlagdWebProvider({
+    host: 'localhost',
+    port: 8013,
+  })
+);
 assert(
   OpenFeature.providerMetadata.name === FLAGD_NAME,
   new Error(`Expected ${FLAGD_NAME} provider to be configured, instead got: ${OpenFeature.providerMetadata.name}`)
