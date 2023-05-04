abfb3d3e8d18ba239e43013583b4bf680ec6bb2f
realanna <anna.reale@dynatrace.com>
2023-05-04 16:12:55 +0200

    test: hacky fix
    Signed-off-by: realanna <anna.reale@dynatrace.com>

--- packages/client/test/events.spec.ts
+++ packages/client/test/events.spec.ts
@@ -1,4 +1,10 @@
-import { ApiEvents, OpenFeature, Provider, ProviderEvents, OpenFeatureEventEmitter } from '../src';
+import {
+  ApiEvents,
+  Provider,
+  ProviderEvents,
+  OpenFeatureEventEmitter,
+  OpenFeatureAPI,
+} from '../src';
 
 const ERROR_REASON = 'error';
 const ERROR_CODE = 'MOCKED_ERROR';
@@ -26,15 +32,19 @@ const ERROR_MOCK_PROVIDER = {
   events: new OpenFeatureEventEmitter(),
 } as unknown as Provider;
 
+
+
 describe('Events', () => {
   // set timeouts short for this suite.
   jest.setTimeout(1000);
+  let API = new (OpenFeatureAPI as any)();
 
   afterEach(() => {
     jest.clearAllMocks();
+    API = new (OpenFeatureAPI as any)();
   });
 
-  describe('Requirement 5.1.1', () => {
+  describe('Requirement 5.1.1, 5.1.3', () => {
     it('The provider defines a mechanism for signalling the occurrence of an event`PROVIDER_READY`', (done) => {
       const myProvider: Provider = {
         metadata: {
@@ -48,33 +58,18 @@ describe('Events', () => {
 
       myProvider.events?.addListener(ProviderEvents.Ready, () => {
         try {
-          expect(OpenFeature.providerMetadata.name).toBe('mock-events');
+          expect(API.providerMetadata.name).toBe('mock-events');
           expect(myProvider.initialize).toHaveBeenCalled();
           done();
         } catch (err) {
           done(err);
         }
       });
-      OpenFeature.setProvider(myProvider);
-    });
-
-    it('It defines a mechanism for signalling `PROVIDER_ERROR`', (done) => {
-
-      //make sure an error event is fired when initialize promise reject
-      ERROR_MOCK_PROVIDER.events?.addListener(ProviderEvents.Error, () => {
-        try {
-          expect(OpenFeature.providerMetadata.name).toBe('mock-events-failure');
-          expect(ERROR_MOCK_PROVIDER.initialize).toHaveBeenCalled();
-          done();
-        } catch (err) {
-          done(err);
-        }
-      });
-      OpenFeature.setProvider(ERROR_MOCK_PROVIDER);
+      API.setProvider(myProvider);
     });
 
     it('It defines a mechanism for signalling `PROVIDER_CONFIGURATION_CHANGED`', (done) => {
-      OpenFeature['_apiEvents'] = new OpenFeatureEventEmitter();
+      API['_apiEvents'] = new OpenFeatureEventEmitter();
       const myProvider: Provider = {
         metadata: {
           name: 'mock-events',
@@ -84,75 +79,85 @@ describe('Events', () => {
         }),
         events: new OpenFeatureEventEmitter(),
       } as unknown as Provider;
-
-      OpenFeature['_apiEvents'].addListener(ApiEvents.ProviderChanged, () => {
+      API['_apiEvents'].addListener(ApiEvents.ProviderChanged, () => {
         try {
-          expect(OpenFeature.providerMetadata.name).toBe('mock-events');
-          expect(myProvider.initialize).toHaveBeenCalled();
+          expect(API.providerMetadata.name).toBe('mock-events');
           done();
         } catch (err) {
           done(err);
         }
       });
-      OpenFeature.setProvider(myProvider);
-    });
-  });
-  describe('Requirement 5.2.1, 5.2.3, 5.2.4, 5.2.5, 5.2.6 ', () => {
-    it('The `client` MUST provide an `addHandler` function for attaching callbacks to `provider events`, which accepts event type(s) and a `event handler function`.', (done) => {
+      API.setProvider(myProvider);
 
-      const client = OpenFeature.getClient();
-      client.addHandler(ProviderEvents.ConfigurationChanged, () => {
-        done();
+    });
+
+    it('It defines a mechanism for signalling `PROVIDER_ERROR`', (done) => {
+      //make sure an error event is fired when initialize promise reject
+      ERROR_MOCK_PROVIDER.events?.addListener(ProviderEvents.Error, () => {
+        try {
+          expect(API.providerMetadata.name).toBe('mock-events-failure');
+          expect(ERROR_MOCK_PROVIDER.initialize).toHaveBeenCalled();
+          done();
+        } catch (err) {
+          done(err);
+        }
       });
-      OpenFeature.setProvider(MOCK_PROVIDER);
+      API.setProvider(ERROR_MOCK_PROVIDER);
     });
 
+  });
 
-    it('If the provider `initialize` function terminates normally, `PROVIDER_READY` handlers MUST run', (done) => {
-
-      const client = OpenFeature.getClient();
+  describe('Requirement 5.2.1, 5.2.3, 5.2.4, 5.2.5 ', () => {
+    it('The `client` MUST provide an `addHandler` function for attaching callbacks to `provider events`, which accepts event type(s) and a `event handler function`.', (done) => {
+      const client = API.getClient();
       client.addHandler(ProviderEvents.Ready, () => {
         done();
       });
-      OpenFeature.setProvider(MOCK_PROVIDER);
-
+      API.setProvider(MOCK_PROVIDER);
     });
 
+    it('If the provider `initialize` function terminates normally, `PROVIDER_READY` handlers MUST run', (done) => {
+      const client = API.getClient();
+      client.addHandler(ProviderEvents.Ready, () => {
+        done();
+      });
+      API.setProvider(MOCK_PROVIDER);
+    });
 
     it('If the provider `initialize` function terminates abnormally, `PROVIDER_ERROR` handlers MUST run.', (done) => {
-
-      const client = OpenFeature.getClient();
+      const client = API.getClient();
       client.addHandler(ProviderEvents.Error, () => {
         done();
       });
-      OpenFeature.setProvider(ERROR_MOCK_PROVIDER);
-
+      API.setProvider(ERROR_MOCK_PROVIDER);
     });
 
     it('`PROVIDER_READY` handlers added after the provider is already in a ready state MUST run immediately.', (done) => {
-
-      const client = OpenFeature.getClient();
+      const client = API.getClient();
 
       MOCK_PROVIDER.events?.addListener(ProviderEvents.Ready, () => {
         try {
           expect(MOCK_PROVIDER.initialize).toHaveBeenCalled();
           jest.setTimeout(10);
-          let count=0;
+          let count = 0;
           client.addHandler(ProviderEvents.Ready, () => {
-            if (count==0) {count++;  done();}
+            if (count == 0) {
+              count++;
+              done();
+            }
           });
         } catch (err) {
           done(err);
         }
       });
 
-      OpenFeature.setProvider(MOCK_PROVIDER);
-
+      API.setProvider(MOCK_PROVIDER);
     });
+  });
 
+  describe('Requirement 5.2.6 ', () => {
     it('Event handlers MUST persist across `provider` changes.', (done) => {
-
-      const client = OpenFeature.getClient();
+      const client = API.getClient();
 
       const myProvider: Provider = {
         metadata: {
@@ -164,21 +169,20 @@ describe('Events', () => {
         events: new OpenFeatureEventEmitter(),
       } as unknown as Provider;
 
-      OpenFeature.setProvider(myProvider);
+      API.setProvider(myProvider);
       let counter = 0;
       client.addHandler(ProviderEvents.Ready, () => {
-        if (OpenFeature.providerMetadata.name== myProvider.metadata.name){
-          OpenFeature.setProvider(MOCK_PROVIDER);
-          counter ++;
+        if (API.providerMetadata.name == myProvider.metadata.name) {
+          API.setProvider(MOCK_PROVIDER);
+          counter++;
         } else {
           expect(counter).toBeGreaterThan(0);
-          expect(OpenFeature.providerMetadata.name).toBe(MOCK_PROVIDER.metadata.name);
-          if (counter==1) {done();}
+          expect(API.providerMetadata.name).toBe(MOCK_PROVIDER.metadata.name);
+          if (counter == 1) {
+            done();
+          }
         }
-
       });
-
     });
-
   });
 });
