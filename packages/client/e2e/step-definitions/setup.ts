51ad360c3f8fac8ad84e62360a75ee97dc4c7e92
Todd Baert <todd.baert@dynatrace.com>
2023-07-26 15:03:18 -0400

    fixup: client e2e
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- packages/client/e2e/step-definitions/setup.ts
+++ packages/client/e2e/step-definitions/setup.ts
@@ -6,14 +6,15 @@ const FLAGD_NAME = 'flagd-web';
 
 // register the flagd provider before the tests.
 console.log('Setting flagd web provider...');
-OpenFeature.setProvider(
-  new FlagdWebProvider({
-    host: 'localhost',
-    port: 8013,
-    tls: false,
-    maxRetries: -1,
-  })
-);
+const provider = new FlagdWebProvider({
+  host: 'localhost',
+  port: 8013,
+  tls: false,
+  maxRetries: -1,
+});
+// TODO: remove this when provider updated to properly use state.
+provider.initialize({});
+OpenFeature.setProvider(provider);
 assert(
   OpenFeature.providerMetadata.name === FLAGD_NAME,
   new Error(`Expected ${FLAGD_NAME} provider to be configured, instead got: ${OpenFeature.providerMetadata.name}`)
