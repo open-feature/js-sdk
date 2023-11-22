0bcf61df69dde2f468417910feb0fa57aceea8d9
Bill <billl@spotify.com>
2023-11-22 15:46:36 +0000

    fix: add Provider Not Ready Error
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- packages/shared/src/errors/index.ts
+++ packages/shared/src/errors/index.ts
@@ -5,3 +5,4 @@ export * from './type-mismatch-error';
 export * from './targeting-key-missing-error';
 export * from './invalid-context-error';
 export * from './open-feature-error-abstract';
+export * from './provider-not-ready-error';
