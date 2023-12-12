23d14aade82d25132714fd3be108cd91c9c15f49
Michael Beemer <beeme1mr@users.noreply.github.com>
2023-12-12 11:38:43 -0500

    feat: add named provider metadata accessor (#715)
    ## This PR
    
    - adds an accessor for named provider metadata
    
    ### Related Issues
    
    Fixes #714 
    
    ### Notes
    
    The default metadata is returned if the name doesn't match a registered
    provider.
    
    This is another example where provider namespace would work well. It
    doesn't make sense to call this "named client" since we are only
    interested in the client.
    
    ---------
    
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>
    Co-authored-by: Todd Baert <todd.baert@dynatrace.com>

--- packages/shared/src/open-feature.ts
+++ packages/shared/src/open-feature.ts
@@ -50,11 +50,21 @@ export abstract class OpenFeatureCommonAPI<P extends CommonProvider = CommonProv
   }
 
   /**
-   * Get metadata about registered provider.
+   * Get metadata about the default provider.
    * @returns {ProviderMetadata} Provider Metadata
    */
   get providerMetadata(): ProviderMetadata {
-    return this._defaultProvider.metadata;
+    return this.getProviderMetadata();
+  }
+
+  /**
+   * Get metadata about a registered provider using the client name.
+   * An unbound or empty client name will return metadata from the default provider.
+   * @param {string} [clientName] The name to identify the client
+   * @returns {ProviderMetadata} Provider Metadata
+   */
+  getProviderMetadata(clientName?: string): ProviderMetadata {
+    return this.getProviderForClient(clientName).metadata;
   }
 
   /**
