68b0f26ccfedc9b1fac32147672f36aa1eb911b2
Todd Baert <todd.baert@dynatrace.com>
2024-01-11 15:10:22 -0500

    feat: suspense support, client scoping, context-sensitive re-rendering (#698)
    Adds a few react features:
    
    - `<Suspense />` support: components using feature flags will trigger
    suspense for easy loaders/spinners
    - Ability to specify name for provider scope: `<OpenFeatureProvider
    name="my-provider">`
    
    > [!IMPORTANT]  
    > Please see
    [here](https://github.com/open-feature/react-test-app/pull/2) for the
    latest changes to the demo application using these features (note that
    we won't be able to merge these demos until this is released).
    
    > [!IMPORTANT]  
    > Also note I've added no tests, which is certainly not my MO. I will
    add them to this PR once there's an agreement on this behavior and
    implementation.
    
    gif from the demo app:
    
    
    ![demo](https://github.com/open-feature/js-sdk/assets/25272906/73007cff-0d0c-44e8-a34c-b99412556206)
    
    :warning: I want to add another feature here to support re-rendering on
    context changes. That requires
    [this](https://github.com/open-feature/js-sdk/pull/731) to be merged.
    
    ---------
    
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>
    Co-authored-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- packages/react/src/use-feature-flag.ts
+++ packages/react/src/use-feature-flag.ts
@@ -1,21 +1,81 @@
-import { Client, EvaluationDetails, FlagValue, ProviderEvents } from '@openfeature/web-sdk';
-import { useEffect, useState } from 'react';
+import { Client, EvaluationDetails, FlagEvaluationOptions, FlagValue, ProviderEvents, ProviderStatus } from '@openfeature/web-sdk';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { useOpenFeatureClient } from './provider';
 
-export function useFeatureFlag<T extends FlagValue>(flagKey: string, defaultValue: T): EvaluationDetails<T> {
-  const [, setForceUpdateState] = useState({});
+type ReactFlagEvaluationOptions = {
+  /**
+   * Suspend flag evaluations while the provider is not ready.
+   * Set to false if you don't want to use React Suspense API.
+   * Defaults to true.
+   */
+  suspend?: boolean,
+  /**
+   * Update the component if the provider emits a ConfigurationChanged event.
+   * Set to false to prevent components from re-rendering when flag value changes
+   * are received by the associated provider.
+   * Defaults to true.
+   */
+  updateOnConfigurationChanged?: boolean,
+  /**
+   * Update the component when the OpenFeature context changes.
+   * Set to false to prevent components from re-rendering when attributes which 
+   * may be factors in flag evaluation change.
+   * Defaults to true.
+   */
+  updateOnContextChanged?: boolean,
+} & FlagEvaluationOptions;
 
+const DEFAULT_OPTIONS: ReactFlagEvaluationOptions = {
+  updateOnContextChanged: true,
+  updateOnConfigurationChanged: true,
+  suspend: true,
+};
+
+enum SuspendState {
+  Pending,
+  Success,
+  Error
+}
+
+/**
+ * Evaluates a feature flag, returning evaluation details.
+ * @param {string}flagKey the flag identifier
+ * @param {T} defaultValue the default value
+ * @param {ReactFlagEvaluationOptions} options options for this evaluation
+ * @template T flag type
+ * @returns { EvaluationDetails<T>} a EvaluationDetails object for this evaluation
+ */
+export function useFeatureFlag<T extends FlagValue>(flagKey: string, defaultValue: T, options?: ReactFlagEvaluationOptions): EvaluationDetails<T> {
+  const defaultedOptions = { ...DEFAULT_OPTIONS, ...options };
+  const [, updateState] = useState<object | undefined>();
+  const forceUpdate = () => {
+    updateState({});
+  };
   const client = useOpenFeatureClient();
 
   useEffect(() => {
-    const forceUpdate = () => setForceUpdateState({});
 
-    // adding handlers here means that an update is triggered, which leads to the change directly reflecting in the UI
-    client.addHandler(ProviderEvents.Ready, forceUpdate);
-    client.addHandler(ProviderEvents.ConfigurationChanged, forceUpdate);
+    if (client.providerStatus !== ProviderStatus.READY) {
+      // update when the provider is ready
+      client.addHandler(ProviderEvents.Ready, forceUpdate);
+      if (defaultedOptions.suspend) {
+        suspend(client, updateState);
+      }
+    }
+
+    if (defaultedOptions.updateOnContextChanged) {
+      // update when the context changes
+      client.addHandler(ProviderEvents.ContextChanged, forceUpdate);
+    }
+
+    if (defaultedOptions.updateOnConfigurationChanged) {
+      // update when the provider configuration changes
+      client.addHandler(ProviderEvents.ConfigurationChanged, forceUpdate);
+    }
     return () => {
-      // be sure to cleanup the handlers
+      // cleanup the handlers (we can do this unconditionally with no impact)
       client.removeHandler(ProviderEvents.Ready, forceUpdate);
+      client.removeHandler(ProviderEvents.ContextChanged, forceUpdate);
       client.removeHandler(ProviderEvents.ConfigurationChanged, forceUpdate);
     };
   }, [client]);
@@ -34,3 +94,61 @@ function getFlag<T extends FlagValue>(client: Client, flagKey: string, defaultVa
     return client.getObjectDetails(flagKey, defaultValue) as EvaluationDetails<T>;
   }
 }
+
+/**
+ * Suspend function. If this runs, components using the calling hook will be suspended.
+ * @param {Client} client the OpenFeature client
+ * @param {Function} updateState the state update function
+ */
+function suspend(client: Client, updateState: Dispatch<SetStateAction<object | undefined>>) {
+  let suspendResolver: () => void;
+  let suspendRejecter: () => void;
+  const suspendPromise = new Promise<void>((resolve) => {
+    suspendResolver = () => {
+      resolve();
+      client.removeHandler(ProviderEvents.Ready, suspendResolver); // remove handler once it's run
+    };
+    suspendRejecter = () => {
+      resolve(); // we still resolve here, since we don't want to throw errors
+      client.removeHandler(ProviderEvents.Error, suspendRejecter); // remove handler once it's run
+    };
+    client.addHandler(ProviderEvents.Ready, suspendResolver);
+    client.addHandler(ProviderEvents.Error, suspendRejecter);
+  });
+  updateState(suspenseWrapper(suspendPromise));
+}
+
+/**
+ * Promise wrapper that throws unresolved promises to support React suspense.
+ * @param {Promise<T>} promise to wrap
+ * @template T flag type
+ * @returns {Function} suspense-compliant lambda
+ */
+function suspenseWrapper <T>(promise: Promise<T>) {
+  let status: SuspendState = SuspendState.Pending;
+  let result: T;
+
+  const suspended = promise.then(
+    (value) => {
+      status = SuspendState.Success;
+      result = value;
+    },
+    (error) => {
+      status = SuspendState.Error;
+      result = error;
+    }
+  );
+
+  return () => {
+    switch (status) {
+      case SuspendState.Pending:
+        throw suspended;
+      case SuspendState.Success:
+        return result;
+      case SuspendState.Error:
+        throw result;
+      default:
+        throw new Error('Suspending promise is in an unknown state.');
+    }
+  };
+};
