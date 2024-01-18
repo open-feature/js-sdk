80a9ba1e5120f1adaadb7b98f5f88b9f03b02682
Lukas Reining <lukas.reining@codecentric.de>
2024-01-18 21:25:26 +0100

    feat: add logger, event handlers and hooks to nest sdk (#761)
    <!-- Please use this template for your pull request. -->
    <!-- Please use the sections that you need and delete other sections -->
    
    ## This PR
    <!-- add the description of the PR here -->
    
    Adds options for logger, event handlers and hooks to Nest SDK
    initialization.
    This change makes the SDK completely configurable during the
    initialization without the need to explicitly call the OpenFeature API.
    
    ### Related Issues
    <!-- add here the GitHub issue that this PR resolves if applicable -->
    
    Relates to #705
    
    ---------
    
    Signed-off-by: Lukas Reining <lukas.reining@codecentric.de>
    Signed-off-by: Luiz Guilherme Ribeiro <ltrindaderibeiro@gmail.com>
    Signed-off-by: Luiz Guilherme Ribeiro <luizgribeiro@ufrj.br>
    Co-authored-by: Luiz Guilherme Ribeiro <ltrindaderibeiro@gmail.com>

--- packages/nest/test/open-feature.module.spec.ts
+++ packages/nest/test/open-feature.module.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getOpenFeatureClientToken, OpenFeatureModule } from '../src';
-import { OpenFeatureClient } from '@openfeature/server-sdk';
+import { getOpenFeatureClientToken, OpenFeatureModule, ServerProviderEvents } from '../src';
+import { OpenFeature, OpenFeatureClient } from '@openfeature/server-sdk';
 import { getOpenFeatureDefaultTestModule } from './fixtures';
 
 describe('OpenFeatureModule', () => {
@@ -46,6 +46,47 @@ describe('OpenFeatureModule', () => {
       const client = moduleRef.get<OpenFeatureClient>(getOpenFeatureClientToken('namedClient'));
       expect(client).toBeDefined();
       expect(await client.getStringValue('testStringFlag', '')).toEqual('expected-string-value-named');
+    });
+  });
+
+  describe('handlers', () => {
+    let moduleWithoutProvidersRef: TestingModule;
+    const handlerSpy = jest.fn();
+
+    beforeAll(async () => {
+      moduleWithoutProvidersRef = await Test.createTestingModule({
+        imports: [OpenFeatureModule.forRoot({ handlers: [[ServerProviderEvents.Ready, handlerSpy]] })],
+      }).compile();
+    });
+
+    it('should add event handlers to OpenFeature', async () => {
+      expect(OpenFeature.getHandlers(ServerProviderEvents.ConfigurationChanged)).toHaveLength(0);
+      expect(OpenFeature.getHandlers(ServerProviderEvents.Stale)).toHaveLength(0);
+      expect(OpenFeature.getHandlers(ServerProviderEvents.Error)).toHaveLength(0);
+      expect(OpenFeature.getHandlers(ServerProviderEvents.Ready)).toHaveLength(1);
+    });
+
+    afterAll(async () => {
+      await moduleWithoutProvidersRef.close();
+    });
+  });
+
+  describe('hooks', () => {
+    let moduleWithoutProvidersRef: TestingModule;
+    const hook = { before: jest.fn() };
+
+    beforeAll(async () => {
+      moduleWithoutProvidersRef = await Test.createTestingModule({
+        imports: [OpenFeatureModule.forRoot({ hooks: [hook] })],
+      }).compile();
+    });
+
+    it('should add hooks to OpenFeature', async () => {
+      expect(OpenFeature.getHooks()).toEqual([hook]);
+    });
+
+    afterAll(async () => {
+      await moduleWithoutProvidersRef.close();
     });
   });
 });
