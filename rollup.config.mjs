9926d233a0010bb50aa9179c762e3d58a086474e
Todd Baert <todd.baert@dynatrace.com>
2024-01-31 11:48:23 -0500

    fix: removed duped core types
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,6 @@
 // this config rolls up all the types in the project to a single declaration (d.ts) file.
 // we do NOT use rollup to build (we use esbuild for that)
 
-import alias from '@rollup/plugin-alias';
 import dts from 'rollup-plugin-dts';
 
 export default {
@@ -18,6 +17,7 @@ export default {
     }
   ],
   plugins: [
-    dts({tsconfig: './tsconfig.json'}),
+    // use the rollup override tsconfig (applies equivalent in each sub-packages as well)
+    dts({tsconfig: './tsconfig.rollup.json'}),
   ],
 };
