b360c2336df0ddd7a7f295d4cb42b261746d6a04
Will Chou <w.chou06@gmail.com>
2024-10-06 14:34:47 -0400

    handle domain and global
    Signed-off-by: Will Chou <w.chou06@gmail.com>

--- packages/react/src/provider/use-context-mutator.ts
+++ packages/react/src/provider/use-context-mutator.ts
@@ -2,28 +2,22 @@ import { OpenFeature, EvaluationContext } from '@openfeature/web-sdk';
 
 type DomainContextMutator = (domain: string, updatedContext: EvaluationContext) => Promise<void>; 
 
-type ContextMutatorReturn = {
-  mutateContext: DomainContextMutator;
-}
 /**
  * 
  * A hook for accessing context mutating functions.
  * 
- * @returns {ContextMutatorReturn} 
  */
-export function useContextMutator(): {
-  mutateContext: DomainContextMutator;
-} {
-  return {
-    /**
-     * 
-     * Mutates the evaluation context for a given domain.
-     * 
-     * @param {string}domain
-     * @param {EvaluationContext} updatedContext
-     */
-    mutateContext: async (domain: string, updatedContext: EvaluationContext) => {
-      await OpenFeature.setContext(domain, updatedContext);
+export function useContextMutator() {
+
+  async function mutateContext(domainOrUpdatedContext: string | EvaluationContext, updatedContextOrUndefined?: EvaluationContext): Promise<void> {
+    if (typeof domainOrUpdatedContext === 'string' && updatedContextOrUndefined) {
+      await OpenFeature.setContext(domainOrUpdatedContext, updatedContextOrUndefined);
+    } else if (typeof domainOrUpdatedContext !== 'string') {
+      OpenFeature.setContext(domainOrUpdatedContext);
     }
+  }
+  
+  return {
+    mutateContext, 
   };
 }
