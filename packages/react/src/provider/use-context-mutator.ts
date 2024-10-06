83836bcc73b3b4562a95f3bd3cf458ba3daf4886
Will Chou <w.chou06@gmail.com>
2024-10-06 14:21:36 -0400

    js doc
    Signed-off-by: Will Chou <w.chou06@gmail.com>

--- packages/react/src/provider/use-context-mutator.ts
+++ packages/react/src/provider/use-context-mutator.ts
@@ -1,7 +1,27 @@
 import { OpenFeature, EvaluationContext } from '@openfeature/web-sdk';
 
-export function useContextMutator() {
+type DomainContextMutator = (domain: string, updatedContext: EvaluationContext) => Promise<void>; 
+
+type ContextMutatorReturn = {
+  mutateContext: DomainContextMutator;
+}
+/**
+ * 
+ * A hook for accessing context mutating functions.
+ * 
+ * @returns {ContextMutatorReturn} 
+ */
+export function useContextMutator(): {
+  mutateContext: DomainContextMutator;
+} {
   return {
+    /**
+     * 
+     * Mutates the evaluation context for a given domain.
+     * 
+     * @param {string}domain
+     * @param {EvaluationContext} updatedContext
+     */
     mutateContext: async (domain: string, updatedContext: EvaluationContext) => {
       await OpenFeature.setContext(domain, updatedContext);
     }
