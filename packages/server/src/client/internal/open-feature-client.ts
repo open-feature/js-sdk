03b0c4a64093b2c5d3974d216f24cd1d173f026d
MarkoMlakar <marko.mlakar@dynatrace.com>
2025-12-11 12:16:54 +0100

    Ensure accumulated hook context is available for the before hook
    * Reverted the purposed changes to ensure the before hook gets the accumulated hook context
    * Replaced the for loop with a single shared object update
    
    Signed-off-by: MarkoMlakar <marko.mlakar@dynatrace.com>

--- packages/server/src/client/internal/open-feature-client.ts
+++ packages/server/src/client/internal/open-feature-client.ts
@@ -338,17 +338,14 @@ export class OpenFeatureClient implements Client {
       const hookContextIndex = hooks.length - 1 - index; // reverse index for before hooks
       const hookContext = hookContexts[hookContextIndex];
 
+      // Update the context on the stable hook context object
+      Object.assign(hookContext.context, accumulatedContext);
+
       const hookResult = await hook?.before?.(hookContext, Object.freeze(options.hookHints));
       if (hookResult) {
         Object.assign(accumulatedContext, hookResult);
-
-        for (let i = 0; i < hooks.length; i++) {
-          Object.assign(hookContexts[hookContextIndex].context, accumulatedContext);
-        }
+        Object.assign(hookContext.context, accumulatedContext);
       }
-
-      // Update the context on the stable hook context object
-      Object.assign(hookContext.context, accumulatedContext);
     }
 
     // after before hooks, freeze the EvaluationContext.
