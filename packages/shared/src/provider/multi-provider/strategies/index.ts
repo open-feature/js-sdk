ac852d2089207f27e0e11eb0eec78827b8f0f8a5
Todd Baert <todd.baert@dynatrace.com>
2026-01-16 14:56:48 -0500

    fixup: remove commented code
    Removed commented-out export statements for clarity.
    
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- packages/shared/src/provider/multi-provider/strategies/index.ts
+++ packages/shared/src/provider/multi-provider/strategies/index.ts
@@ -2,15 +2,3 @@ export * from './base-evaluation-strategy';
 export { FirstMatchStrategy as BaseFirstMatchStrategy } from './first-match-strategy';
 export { FirstSuccessfulStrategy as BaseFirstSuccessfulStrategy } from './first-successful-strategy';
 export { ComparisonStrategy as BaseComparisonStrategy } from './comparison-strategy';
-
-// // Export types and base class from base-evaluation-strategy
-// export {
-//   BaseEvaluationStrategy,
-//   type StrategyEvaluationContext,
-//   type BaseStrategyProviderContext,
-//   type BaseStrategyPerProviderContext,
-//   type BaseProviderResolutionResult,
-//   type BaseProviderResolutionSuccessResult,
-//   type BaseProviderResolutionErrorResult,
-//   type BaseFinalResult,
-// } from './base-evaluation-strategy';
