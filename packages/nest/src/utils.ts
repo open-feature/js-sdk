903a553e55c5b8b8dcbfb8dd30abeff866438d99
Benjamin Kenawell <ben@withremark.com>
2026-05-14 15:46:47 -0400

    add tests for getRequestFromContext
    Signed-off-by: Benjamin Kenawell <ben@withremark.com>

--- packages/nest/src/utils.ts
+++ packages/nest/src/utils.ts
@@ -1,12 +1,12 @@
 import { withFrameworkMetadata } from '@openfeature/core';
 import type { Client, EvaluationContext } from '@openfeature/server-sdk';
 import { OpenFeature } from '@openfeature/server-sdk';
-import { ExecutionContext } from '@nestjs/common';
+import type { ExecutionContext } from '@nestjs/common';
 import type { GqlExecutionContext as GqlExecutionContextClass, GqlContextType } from '@nestjs/graphql';
 
 let GqlExecutionContext: typeof GqlExecutionContextClass | undefined;
 try {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/consistent-type-imports
   ({ GqlExecutionContext } = require('@nestjs/graphql') as typeof import('@nestjs/graphql'));
 } catch {
   // @nestjs/graphql is an optional peer dependency
@@ -26,9 +26,11 @@ export function getClientForEvaluation(domain?: string, context?: EvaluationCont
 }
 
 /**
+ * @template T type of the returned request. Likely a Express or Fastify request
  * @param {ExecutionContext} context NestJS's execution context
- * @returns request object
+ * @returns {T | undefined} request object
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getRequestFromContext = <T = any>(context: ExecutionContext): T | undefined => {
   if (context.getType() === 'http') {
     return context.switchToHttp().getRequest<T>();
